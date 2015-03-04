2df3fce4151aa71e5fb6c11cab4a35ec70c109f2
Yoann Fuks <yoannfuks@hotmail.fr>
2015-03-04 04:24:02 +0100

    Change color to 256

--- sources/color.c
+++ sources/color.c
@@ -14,24 +14,18 @@
 
 void		set_colors(void)
 {
-	init_color(COLOR_RED, 400, 400, 400);
-	init_color(COLOR_BLUE, 800, 800, 800);
-	init_pair(2, COLOR_BLACK, COLOR_BLUE);
-	init_color(COLOR_GREEN, 200, 600, 800);
-	init_pair(4, COLOR_BLACK, COLOR_GREEN);
-	init_color(COLOR_YELLOW, 1000, 650, 000);
-	init_pair(8, COLOR_BLACK, COLOR_YELLOW);
-	init_color(COLOR_MAGENTA, 1000, 450, 000);
-	init_pair(16, COLOR_BLACK, COLOR_MAGENTA);
-	init_color(COLOR_CYAN, 1000, 150, 000);
-	init_pair(32, COLOR_BLACK, COLOR_CYAN);
-	init_pair(64, COLOR_BLACK, COLOR_BLUE);
-	init_pair(128, COLOR_BLACK, COLOR_GREEN);
-	init_pair(256, COLOR_BLACK, COLOR_YELLOW);
-	init_pair(512, COLOR_BLACK, COLOR_MAGENTA);
-	init_pair(1024, COLOR_BLACK, COLOR_CYAN);
-	init_pair(2048, COLOR_BLACK, COLOR_CYAN);
-	init_pair(4096, COLOR_BLACK, COLOR_BLUE);
-	init_pair(8192, COLOR_BLACK, COLOR_GREEN);
-	init_pair(16384, COLOR_BLACK, COLOR_YELLOW);
+	init_pair(2, COLOR_BLACK, 230);
+	init_pair(4, COLOR_BLACK, 228);
+	init_pair(8, COLOR_BLACK, 214);
+	init_pair(16, COLOR_BLACK, 202);
+	init_pair(32, COLOR_BLACK, 160);
+	init_pair(64, COLOR_BLACK, 88);
+	init_pair(128, COLOR_BLACK, 190);
+	init_pair(256, COLOR_BLACK, 226);
+	init_pair(512, COLOR_BLACK, 15);
+	init_pair(1024, COLOR_BLACK, 123);
+	init_pair(2048, COLOR_BLACK, 51);
+	init_pair(4096, COLOR_BLACK, 48);
+	init_pair(8192, COLOR_WITHE, COLOR_BLACK);
+	init_pair(16384, COLOR_BLACK, 196);
 }
